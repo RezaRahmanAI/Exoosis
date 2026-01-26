6643992f093d5534597173a468b62140fec95392
RezaRahmanAI <rizverahman.ai@gmail.com>
2026-01-26 22:43:24 +0600

    running

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  apiUrl: 'http://localhost:50895/api', // Replace with actual base API URL
-  useMockData: true
+  apiUrl: 'https://localhost:50894/api', // Replace with actual base API URL
+  useMockData: true,
 };
